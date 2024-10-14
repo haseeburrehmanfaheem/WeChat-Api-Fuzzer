ed34fe919cd84d25dfba4df10adfb71ee410a565
Haseeb <23100044@lums.edu.pk>
2024-10-14 01:41:09 -0400

    folder removed

--- Frida-Scripts/app/intent/process.js
+++
@@ -1,8 +0,0 @@
-Java.perform(function() {
-    // Get the current process ID and name
-    var pid = Process.id;
-    var processName = Process.name;
-
-    console.log("Connected to PID: " + pid + ", Process Name: " + processName);
-    console.log(Process);
-});
--- Frida-Scripts/app/undocExploration/callback.js
+++
@@ -1,8 +0,0 @@
-
-Java.perform(function () {
-let AppBrandCommonBindingJni = Java.use("com.tencent.mm.appbrand.commonjni.AppBrandCommonBindingJni");
-AppBrandCommonBindingJni["invokeCallbackHandler"].implementation = function (i15, str) {
-    console.log(`AppBrandCommonBindingJni.invokeCallbackHandler is called: i15=${i15}, str=${str}`);
-    this["invokeCallbackHandler"](i15, str);
-};
-})
